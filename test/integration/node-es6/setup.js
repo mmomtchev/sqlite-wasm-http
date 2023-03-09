11e935abfac87eaa0c4fcaa3ac045cdc9b3f59df
Momtchil Momtchev <momtchil@momtchev.com>
2023-03-09 17:42:17 +0100

    Add node.js

---
+++ test/integration/node-es6/setup.js
@@ -0,0 +1,14 @@
+// Some quick hacks to make SQLite WASM work in Node.js
+import WebWorker from 'web-worker';
+import { MessageChannel, MessagePort as MessagePort } from 'worker_threads';
+import { performance } from 'perf_hooks';
+globalThis.self = globalThis;
+self.location = { href: 'http://localhost/' };
+globalThis.Worker = class Worker extends WebWorker {
+    constructor(specifier, options = {}) {
+        super(specifier, Object.assign(Object.assign({}, options), { type: 'module' }));
+    }
+};
+globalThis.MessagePort = MessagePort;
+globalThis.MessageChannel = MessageChannel;
+globalThis.performance = performance;
