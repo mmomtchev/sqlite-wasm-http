d25799b5bd4339eb2135d124b25b29dedfea3f58
Momtchil Momtchev <momtchil@momtchev.com>
2023-03-09 17:59:58 +0100

    add TypeScript in Node integration target

---
+++ test/integration/node-ts-esm/index.ts
@@ -0,0 +1,40 @@
+import { createSQLiteThread, createHttpBackend } from 'sqlite-wasm-http';
+import './setup.js';
+
+(async function main() {
+  const httpBackend = createHttpBackend({
+    maxPageSize: 1024,
+    timeout: 10000
+  });
+  const db = await createSQLiteThread({ http: httpBackend });
+
+  await db('config-get', {});
+
+  await db('open', {
+    filename: 'file:' + encodeURI('https://velivole.b-cdn.net/maptiler-osm-2017-07-03-v3.6.1-europe.mbtiles'),
+    vfs: 'http'
+  });
+
+  const rows = [];
+
+  await db('exec', {
+    sql: 'SELECT * FROM tiles WHERE zoom_level = 1',
+    callback: function (row) {
+      rows.push(row);
+    }
+  });
+
+  await db('exec', {
+    sql: 'SELECT * FROM tiles WHERE zoom_level = 0',
+    callback: function (row) {
+      rows.push(row);
+    }
+  });
+
+  if (rows.length < 6)
+    throw new Error(('Could not retrieve all rows'));
+
+  await db('close', {});
+  db.close();
+  await httpBackend.close();
+})();
