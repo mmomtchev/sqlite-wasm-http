a5edadd56a721acf6e071736d0683e2fa4133c8c
Momtchil Momtchev <momtchil@momtchev.com>
2023-03-09 21:13:55 +0100

    a somewhat more visual example that actually produces html

--- examples/index.ts
+++ examples/index.ts
@@ -1,5 +1,7 @@
 import { createSQLiteThread, createHttpBackend } from 'sqlite-wasm-http';
 
+const resultsDiv = document.querySelector('#results');
+
 (async function main() {
   const httpBackend = createHttpBackend({
     maxPageSize: 1024,
@@ -7,24 +9,41 @@ import { createSQLiteThread, createHttpBackend } from 'sqlite-wasm-http';
   });
   const db = await createSQLiteThread({ http: httpBackend });
 
-  console.log(await db('config-get', {}));
+  let msg: SQLite.Response;
 
-  console.log(await db('open', {
-    filename: 'file:' + encodeURI('https://velivole.b-cdn.net/maptiler-osm-2017-07-03-v3.6.1-europe.mbtiles'),
+  resultsDiv.innerHTML += '<div>get-config</div>';
+  msg = await db('config-get', {});
+  resultsDiv.innerHTML += `<div>${JSON.stringify(msg)}</div>`;
+
+  const remoteDB = 'https://velivole.b-cdn.net/maptiler-osm-2017-07-03-v3.6.1-europe.mbtiles';
+  resultsDiv.innerHTML += `<div>open ${remoteDB}</div>`;
+  msg = await db('open', {
+    filename: 'file:' + encodeURI(remoteDB),
     vfs: 'http'
-  }));
+  });
+  resultsDiv.innerHTML += `<div>${JSON.stringify(msg)}</div>`;
 
-  console.log(await db('exec', {
-    sql: 'SELECT * FROM tiles WHERE zoom_level = 1',
-    callback: function (row) {
-      console.log('got row', row);
-    }
-  }));
-
-  console.log(await db('exec', {
+  resultsDiv.innerHTML += '<div>SELECT * FROM tiles WHERE zoom_level = 0</div>';
+  msg = await db('exec', {
     sql: 'SELECT * FROM tiles WHERE zoom_level = 0',
     callback: function (row) {
-      console.log('got row', row);
+      if (row.row)
+        resultsDiv.innerHTML += `<div>row: ${JSON.stringify(row)}</div>`;
+      else
+        resultsDiv.innerHTML += `<div>end of rows</div>`;
     }
-  }));
+  });
+  resultsDiv.innerHTML += `<div>${JSON.stringify(msg)}</div>`;
+
+  resultsDiv.innerHTML += '<div>SELECT * FROM tiles WHERE zoom_level = 1</div>';
+  msg = await db('exec', {
+    sql: 'SELECT * FROM tiles WHERE zoom_level = 1',
+    callback: function (row) {
+      if (row.row)
+        resultsDiv.innerHTML += `<div>row: ${JSON.stringify(row)}</div>`;
+      else
+        resultsDiv.innerHTML += `<div>end of rows</div>`;
+    }
+  });
+  resultsDiv.innerHTML += `<div>${JSON.stringify(msg)}</div>`;
 })();
