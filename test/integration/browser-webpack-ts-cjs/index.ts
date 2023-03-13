e07b7284f79da1b43a5f7484ea7371bf03575a8a
Momtchil Momtchev <momtchil@momtchev.com>
2023-03-13 16:13:57 +0100

    add a browser TS transpiled to CJS configuration

---
+++ test/integration/browser-webpack-ts-cjs/index.ts
@@ -0,0 +1,30 @@
+(async function main() {
+  const { createSQLiteThread, createHttpBackend } = await import('sqlite-wasm-http');
+
+  const httpBackend = createHttpBackend({
+    maxPageSize: 1024,
+    timeout: 10000
+  });
+  const db = await createSQLiteThread({ http: httpBackend });
+
+  console.log(await db('config-get', {}));
+
+  console.log(await db('open', {
+    filename: 'file:' + encodeURI('https://velivole.b-cdn.net/maptiler-osm-2017-07-03-v3.6.1-europe.mbtiles'),
+    vfs: 'http'
+  }));
+
+  console.log(await db('exec', {
+    sql: 'SELECT * FROM tiles WHERE zoom_level = 1',
+    callback: function (row) {
+      console.log('got row', row);
+    }
+  }));
+
+  console.log(await db('exec', {
+    sql: 'SELECT * FROM tiles WHERE zoom_level = 0',
+    callback: function (row) {
+      console.log('got row', row);
+    }
+  }));
+})();
