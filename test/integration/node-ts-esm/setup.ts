65200b678622dc3cc65256bb1f1c9953c433da35
Momtchil Momtchev <momtchil@momtchev.com>
2023-03-23 17:52:08 +0100

    enable strict mode in the integration tests

--- test/integration/node-ts-esm/setup.ts
+++ test/integration/node-ts-esm/setup.ts
@@ -1,4 +1,5 @@
 // Some quick hacks to make SQLite WASM work in Node.js
+/// <reference path='./modules.d.ts' />
 import WebWorker from '@mmomtchev/web-worker';
 import { MessageChannel, MessagePort as MessagePort } from 'worker_threads';
 import { performance } from 'perf_hooks';
@@ -7,7 +8,7 @@ import { performance } from 'perf_hooks';
 (self as any).location = { href: 'http://localhost/' };
 
 globalThis.Worker = class Worker extends WebWorker {
-  constructor(specifier, options = {}) {
+  constructor(specifier: string, options = {}) {
     super(specifier, { ...options, type: 'module' });
   }
 } as typeof WebWorker;
