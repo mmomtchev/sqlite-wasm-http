0f437d68b616a360cd7acc8c1f221676fe6376d2
Momtchil Momtchev <momtchil@momtchev.com>
2024-12-17 18:44:22 +0100

    switch all browser tests to chromeheadless

--- test/integration/browser-webpack-ts-esm.karma.cjs
+++ test/integration/browser-webpack-ts-esm.karma.cjs
@@ -24,7 +24,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: true,
     concurrency: Infinity
   });
